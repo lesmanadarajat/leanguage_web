213abc9d4545f30d41927a482c99b7dcab277985
Lesmana Darajat <lesmanadarajat42@gmail.com>
2025-09-16 20:58:38 +0700

    upload files

---
+++ learns/index.php
@@ -0,0 +1,36 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hello world</h1>
+    <?php
+    $teks = "php";
+    echo "ini tulisan $teks yg menggunakan echo <br>";
+    EcHo "ini tulisan $teks yg menggunakan EcHo <br>";
+    ECHO "ini tulisan $teks yg menggunakan ECHO <br>";
+
+    $color = "red";
+    echo "my car $color assep<br>";
+
+    $x = 12;
+    $y = 7;
+    echo "$x + $y<br>";
+
+    var_dump(5);
+    var_dump("Asep");
+    var_dump(3.14);
+    var_dump([1,2,45,7]);
+    var_dump(true);
+    var_dump(null);
+
+    // 3 variabel
+    $x = $y = $z = '<br>lesmana linux';
+    echo $x;
+    echo $y;
+    echo $z;
+    ?>
+</body>
+</html>
