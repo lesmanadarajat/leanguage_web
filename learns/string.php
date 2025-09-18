9526cfef3791ab83451d371213786bc860962f81
Lesmana Darajat <lesmanadarajat42@gmail.com>
2025-09-18 22:15:04 +0700

    upload files

---
+++ learns/string.php
@@ -0,0 +1,17 @@
+<?php
+$x = "Hello nama saya L7 ";
+echo $x;
+echo "<br>";
+
+// ngitung panjang dari string nya
+echo strlen($x);
+echo "<br>";
+
+// menghitung banyak nya kata dlm suatu string
+echo str_word_count($x);
+echo '<br>';
+
+// mencari suatu kata di dlm str, klo ada dia true klo gada di false
+echo strpos($x, 'L7');
+
+?>
