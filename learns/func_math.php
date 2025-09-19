d59454ac2879ca9ed11730e4c478357f8e19fa20
Lesmana Darajat <lesmanadarajat42@gmail.com>
2025-09-19 21:43:16 +0700

    upload files

---
+++ learns/func_math.php
@@ -0,0 +1,22 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo (pi());
+    echo "<br";
+
+    $w = 150;
+    $x = 0;
+    $y = -200;
+    $z = 77;
+    echo (min($y, $w, $x, $z));
+    echo (max($y, $z, $w, $x));
+
+
+    ?>
+</body>
+</html>
