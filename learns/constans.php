3a25a4495138b7005fd4b5d6c746d2874fb77789
Lesmana Darajat <lesmanadarajat42@gmail.com>
2025-09-22 22:34:28 +0700

    upload file

---
+++ learns/constans.php
@@ -0,0 +1,27 @@
+<?php 
+define ('getting', 'Lamborghini');
+echo getting;
+
+echo '<br>';
+
+const nama = "Lesmana";
+echo nama;
+
+echo '<br';
+
+define ("motor",['vario',
+                 'beat',
+                 'supra',
+                 'aerox'
+
+]);
+
+echo motor[0];
+
+echo '<br';
+
+function test() {
+    echo getting;
+}
+test();
+?>
