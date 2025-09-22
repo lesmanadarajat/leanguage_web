3327bcc304b18be29aa987f4c78c167cecd1d8d1
Lesmana Darajat <lesmanadarajat42@gmail.com>
2025-09-22 22:35:01 +0700

    upload file

---
+++ learns/ctrlflow.php
@@ -0,0 +1,30 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$t = date("H");
+
+if ($t < "10") {
+  echo "Have a good morning!";
+} elseif ($t < "20") {
+  echo "Have a good day!";
+} else {
+  echo "Have a good night!";
+}
+
+if (isset($_POST['nama'])) {
+    echo "Halo, " . $_POST['nama'];
+}
+
+?>
+
+<form method="post">
+  Nama: <input type="text" name="nama">
+  <input type="submit" value="Kirim">
+</form>
+</body>
+</html>
